9bb9ab6811948748fefbd2ba48575cf7772a3086
siddhartha <siddhartha.maji@bluehorse.in>
2017-12-27 16:45:18 +0530

    ZxZX

--- app/src/main/java/in/creativelizard/emailsender/adapters/AllTypeListAdapter.kt
+++ app/src/main/java/in/creativelizard/emailsender/adapters/AllTypeListAdapter.kt
@@ -5,6 +5,7 @@ import `in`.creativelizard.emailsender.activities.MainActivity
 import `in`.creativelizard.emailsender.beans.AllTypeItem
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +20,7 @@ import kotlinx.android.synthetic.main.all_type_list_cell.view.*
 class AllTypeListAdapter (private val arrayList: ArrayList<AllTypeItem>,
                           private val context: Context,
                           private val layout: Int) : RecyclerView.Adapter<AllTypeListAdapter.ViewHolder>() {
+    private val to:Array<String>?=null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllTypeListAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(layout, parent, false)
         return ViewHolder(v)
@@ -34,6 +36,20 @@ class AllTypeListAdapter (private val arrayList: ArrayList<AllTypeItem>,
             i.putExtra("id",arrayList[position].id)
             context.startActivity(i)
             true
+        }
+
+        holder.itemView.setOnClickListener {
+            to?.set(0,"siddhartha@creativelizard.in")
+            val i = Intent(Intent.ACTION_VIEW)
+            //i.type = "message/rfc822"
+           // i.data = Uri.parse("mailto:")
+            i.type = "text/plain"
+            i.data = Uri.parse("siddhartha@creativelizard.in")
+            i.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
+            i.putExtra(Intent.EXTRA_EMAIL  ,to)
+            i.putExtra(Intent.EXTRA_SUBJECT, arrayList[position].title)
+            i.putExtra(Intent.EXTRA_TEXT   , arrayList[position].content)
+            context.startActivity(i)
         }
     }
 
